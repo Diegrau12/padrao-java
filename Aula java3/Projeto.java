df4ba795424733f103e0f50fdcefe4f279edb998
Diegrau12 <111328424+Diegrau12@users.noreply.github.com>
2022-10-10 19:39:12 -0400

    Add files via upload

---
+++ Aula java3/Projeto.java
@@ -0,0 +1,8 @@
+public class Projeto {
+    public static void main(String[]args) {
+        System.out.println("Olá Mundo!!");
+
+
+    }
+    
+}
---
+++ Aula java3/ProjetoSymflowerTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+
+public class ProjetoSymflowerTest {
+	@Test
+	public void main1() {
+		String[] args = null;
+		Projeto.main(args);
+	}
+}
